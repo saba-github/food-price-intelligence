9d682be9bfde667fec3aa9a9c1723c584853cd66
Sabahat Şengezer <sabahatsngzr@gmail.com>
2026-04-03 00:16:05 +0300

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from db import get_connection
+import pandas as pd
+
+
+conn = get_connection()
+
+
+kpi = pd.read_sql("""
+select
+    count(distinct standardized_product_name) as product_count,
+    count(*) as observations,
+    round(avg(price_per_unit),2) as avg_price
+from fact_price_observations
+""", conn)
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Products", kpi["product_count"][0])
+col2.metric("Observations", kpi["observations"][0])
+col3.metric("Avg Price/unit", kpi["avg_price"][0])
+
 
 st.set_page_config(
     page_title="Food Price Intelligence",
