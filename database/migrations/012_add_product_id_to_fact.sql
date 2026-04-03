101c92b14990b496ef79c0d58692b8ecb5128e25
Sabahat Şengezer <sabahatsngzr@gmail.com>
2026-04-03 16:58:51 +0300

    Update 012_add_product_id_to_fact.sql

--- database/migrations/012_add_product_id_to_fact.sql
+++ database/migrations/012_add_product_id_to_fact.sql
@@ -1,4 +1,13 @@
-ALTER TABLE fact_price_observations
-ADD CONSTRAINT fk_product
-FOREIGN KEY (product_id)
-REFERENCES dim_products(product_id);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'fk_product'
+    ) THEN
+        ALTER TABLE fact_price_observations
+        ADD CONSTRAINT fk_product
+        FOREIGN KEY (product_id)
+        REFERENCES dim_products(product_id);
+    END IF;
+END $$;
