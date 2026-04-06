68746a4b84f5788737f283826a88290635599369
Sabahat Şengezer <sabahatsngzr@gmail.com>
2026-04-06 21:58:11 +0300

    013_make 013 migration no-op after fixing 012

--- database/migrations/013_fix_fact_product_id.sql
+++ database/migrations/013_fix_fact_product_id.sql
@@ -1,16 +1,2 @@
-ALTER TABLE fact_price_observations
-ADD COLUMN IF NOT EXISTS product_id INTEGER;
-
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM pg_constraint
-        WHERE conname = 'fk_product'
-    ) THEN
-        ALTER TABLE fact_price_observations
-        ADD CONSTRAINT fk_product
-        FOREIGN KEY (product_id)
-        REFERENCES dim_products(product_id);
-    END IF;
-END $$;
+-- This migration is now a no-op.
+-- Logic moved to 012_add_product_id_to_fact.sql
