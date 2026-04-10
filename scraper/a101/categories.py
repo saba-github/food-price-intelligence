0325be06b625781154f26b2baa9c9fb71b15c0a2
Sabahat Şengezer <sabahatsngzr@gmail.com>
2026-04-10 19:35:59 +0300

    Update categories.py

--- scraper/a101/categories.py
+++ scraper/a101/categories.py
@@ -1,1 +1,15 @@
-
+def get_a101_category_products(category_slug: str):
+    return [
+        {
+            "product_id": "a101_1",
+            "product_name": "Domates",
+            "sku": "a101_domates",
+            "shown_price_tl": 29.90,
+        },
+        {
+            "product_id": "a101_2",
+            "product_name": "Salatalık",
+            "sku": "a101_salatalik",
+            "shown_price_tl": 24.90,
+        },
+    ]
