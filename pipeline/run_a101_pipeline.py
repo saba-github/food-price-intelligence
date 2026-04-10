c12800b290406d80e8c92745917319971fb1f427
Sabahat Şengezer <sabahatsngzr@gmail.com>
2026-04-10 19:36:31 +0300

    Update run_a101_pipeline.py

--- pipeline/run_a101_pipeline.py
+++ pipeline/run_a101_pipeline.py
@@ -1,1 +1,9 @@
+from scraper.a101.categories import get_a101_category_products
+from config.retailers import RETAILER_CONFIG
 
+def run_pipeline(category_key: str):
+    category_slug = RETAILER_CONFIG["a101"]["categories"][category_key]
+
+    products = get_a101_category_products(category_slug)
+
+    print("A101 products:", products)
