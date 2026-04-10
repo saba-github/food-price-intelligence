f23a013071a9ff263bb99ea479c69e1fc63e37d2
Sabahat Şengezer <sabahatsngzr@gmail.com>
2026-04-10 18:00:04 +0300

    Create retailers.py

---
+++ config/retailers.py
@@ -0,0 +1,11 @@
+RETAILER_CONFIG = {
+    "migros": {
+        "source_name": "migros",
+        "currency": "TRY",
+        "categories": {
+            "fruit_veg": "meyve-sebze-c-2",
+            "dairy_breakfast": "sut-kahvaltilik-c-4",
+            "beverages": "icecek-c-6",
+        },
+    }
+}
